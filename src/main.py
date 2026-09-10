fb27ed54644dc2e39cfec140a93585241a02cda5
wairua2023 <wairuasteve@gmail.com>
2026-09-10 17:06:34 +0300

    Update main and utils

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 
 
-from utils import square, is_even, celsius_to_fahrenheit
+from utils import square, is_even, celsius_to_fahrenheit,greet
 #Enter a number and check if it's even or odd, and also convert Celsius to Fahrenheit 
 number = float(input("Enter a number: "))
 print(f"Square: {square(number)}")
 print(f"Even: {is_even(number)}")
 print(f"Celcius: {celsius_to_fahrenheit(number)}")
+
+print(greet {Steve})
